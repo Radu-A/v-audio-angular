497fcad9db83e8bdf4d594a9fee523be1e12a26d
Radu-A <elcastravete@gmail.com>
2026-01-07 23:27:25 +0100

    feat(header): add header elements and style

--- client/src/app/layout/footer/footer.ts
+++ client/src/app/layout/footer/footer.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  LucideAngularModule,
+  Instagram,
+  Facebook,
+  Youtube,
+  X,
+  Globe,
+  ShieldCheck,
+  CreditCard,
+} from 'lucide-angular';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {
-
+  readonly Globe = inject(Globe);
+  // socialLinks = [
+  //   { icon: Instagram, url: 'https://instagram.com' },
+  //   { icon: Facebook, url: 'https://facebook.com' },
+  //   { icon: Youtube, url: 'https://youtube.com' },
+  // ];
 }
