a34a2a4eab0fe7b32422abcaa8073c7103fc9e80
Radu-A <elcastravete@gmail.com>
2026-01-09 19:05:29 +0100

    feat(footer): add html, css and script to footer

--- client/src/app/layout/footer/footer.ts
+++ client/src/app/layout/footer/footer.ts
@@ -1,26 +1,57 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import {
   LucideAngularModule,
   Instagram,
+  Twitter,
   Facebook,
   Youtube,
-  X,
-  Globe,
   ShieldCheck,
+  Globe,
   CreditCard,
 } from 'lucide-angular';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {
-  readonly Globe = inject(Globe);
-  // socialLinks = [
-  //   { icon: Instagram, url: 'https://instagram.com' },
-  //   { icon: Facebook, url: 'https://facebook.com' },
-  //   { icon: Youtube, url: 'https://youtube.com' },
-  // ];
+  readonly ShieldCheck = ShieldCheck;
+  readonly Globe = Globe;
+  readonly CreditCard = CreditCard;
+  socialIcons = [
+    { FileIcon: Instagram, url: 'https://www.instagram.com/' },
+    { FileIcon: Twitter, url: 'https://x.com/?lang=es' },
+    { FileIcon: Facebook, url: 'https://www.facebook.com/' },
+    { FileIcon: Youtube, url: 'https://www.youtube.com/' },
+  ];
+
+  footerLinks = [
+    {
+      title: 'Explorar',
+      links: [
+        'Auriculares Over-Ear',
+        'In-Ear True Wireless',
+        'Altavoces Bluetooth',
+        'Soundbars',
+        'Ver Novedades',
+      ],
+    },
+    {
+      title: 'Ayuda',
+      links: [
+        'Estado del pedido',
+        'Envíos y Devoluciones',
+        'Aura Care+ (Garantía)',
+        'Centro de Soporte',
+        'Contactar',
+      ],
+    },
+    {
+      title: 'Empresa',
+      links: ['Sobre Nosotros', 'Sostenibilidad', 'Prensa', 'Inversores', 'Carreras'],
+    },
+  ];
 }
