0ce83ac3d236be04d027b9cc75793f1d78741e1a
Radu-A <elcastravete@gmail.com>
2026-01-10 13:08:03 +0100

    feat(product.list): add product model, serve and list view with cors error

--- client/src/app/features/products/product-list/product-list.ts
+++ client/src/app/features/products/product-list/product-list.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { ProductModel } from '../../../models/product.model';
+import { ProductService } from '../../../services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
 export class ProductList {
-
+  serve = inject(ProductService);
+  productList$: Observable<ProductModel[]> = this.serve.getAllProducts();
 }
